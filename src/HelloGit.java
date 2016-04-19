0c408399060f68599e600a3c12bf750b33373b3e
YiningGong <nicolasgongyn@gmail.com>
2016-04-19 20:40:52 +0800

    测试
    增加测试文件

---
+++ src/HelloGit.java
@@ -0,0 +1,9 @@
+/**
+ * Created by gongyining on 2015/10/26.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hell git!");
+        System.out.println("hello git!");
+    }
+}
